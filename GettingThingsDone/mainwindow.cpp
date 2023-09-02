caeb7e39aa359639c14e36c55adafdf1ef672299
eloranta <hannu.eloranta@gmail.com>
2023-09-02 21:31:45 +0300

    fix: database

--- GettingThingsDone/mainwindow.cpp
+++ GettingThingsDone/mainwindow.cpp
@@ -5,14 +5,13 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    QSqlQuery query;
+    query.exec("create table if not exists inbox (Id integer primary key autoincrement, Stuff text)");
+    query.exec("insert into inbox (Stuff) values('xxx')");
+
     ui->setupUi(this);
     ui->tableView->setModel(&model);
     model.setTable("inbox");
-
-    QSqlQuery query;
-    query.exec("create table if not exists inbox (Id integer primary key autoincrement, Stuff text)");
-
-    query.exec("insert into inbox (1, Stuff) values('xxx')");
 }
 
 MainWindow::~MainWindow()
