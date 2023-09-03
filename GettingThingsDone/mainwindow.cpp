01e63a17afe06af31ee04a56d9289169191bea63
eloranta <hannu.eloranta@gmail.com>
2023-09-03 07:03:54 +0300

    add: add button

--- GettingThingsDone/mainwindow.cpp
+++ GettingThingsDone/mainwindow.cpp
@@ -7,12 +7,14 @@ MainWindow::MainWindow(QWidget *parent)
 {
     QSqlQuery query;
     query.exec("create table if not exists inbox (Id integer primary key autoincrement, Stuff text)");
-    query.exec("insert into inbox (Stuff) values('xxx')");
 
     ui->setupUi(this);
     ui->tableView->setModel(&model);
     model.setTable("inbox");
     model.select();
+
+    connect(ui->addButton, SIGNAL(clicked()), this, SLOT(addItem()));
+
 }
 
 MainWindow::~MainWindow()
@@ -20,3 +22,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::addItem()
+{
+    QSqlQuery query;
+    query.exec("insert into inbox (Stuff) values('')");
+    model.select();
+}
--- GettingThingsDone/mainwindow.h
+++ GettingThingsDone/mainwindow.h
@@ -19,5 +19,7 @@ public:
 private:
     Ui::MainWindow *ui;
     QSqlRelationalTableModel model;
+private slots:
+    void addItem();
 };
 #endif // MAINWINDOW_H
